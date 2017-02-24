29bf4b06fe02760c3af1513719652bcb3cce01a1
aluno <aluno@mscgresl00402.CGR.ACAD>
2017-02-23 21:30:44 -0300

    Testando funções - Aula 23-02-2017

---
+++ src/main/java/br/estacio/terenciani/programacao/VariavelTipo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.terenciani.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class VariavelTipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        imprime();
+        
+        System.out.println("Estou brincando de funções");
+        
+        imprime();    
+    }
+    /**
+     *
+     */
+    public static void imprime(){
+        System.out.println("-----");
+        System.out.println("Teste");
+        System.out.println("-----"); 
+    }
+}
