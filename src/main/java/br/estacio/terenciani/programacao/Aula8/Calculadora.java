d8dfc15aba31f1c88942f68a6617abd5c544e0d2
josmar <josmar@note-arch>
2017-03-29 22:14:55 -0400

    Aula 29-03-2017 - Calculadora exercício

--- src/main/java/br/estacio/terenciani/programacao/Aula8/Calculadora.java
+++ src/main/java/br/estacio/terenciani/programacao/Aula8/Calculadora.java
@@ -1,9 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
  */
 package br.estacio.terenciani.programacao.Aula8;
+
+import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -17,35 +20,66 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
+        double primeiroOperando = 0, segundoOperando = 0, resultado = 0;
+        String primeiro, segundo, selectOperacao;
+        int escolha = 0;
+        Object[] operacao = {"+", "-", "*", "/"};
+
+        primeiro:
+        do {
         
-        double primeiroOperando, segundoOperando, resultado=0;
-        String operacao;
+        primeiro = JOptionPane.showInputDialog(null, "Digite o primeiro operando");
         
-        System.out.print("Digite o primeiro operando: ");
-        primeiroOperando = scan.nextDouble();
-        
-        System.out.print("Informe o operador: ");
-        operacao = scan.next();
+            if (primeiro == null) {
+                break primeiro;
                 
-        System.out.print("Digite o segundo operando: ");
-        segundoOperando = scan.nextDouble();
-       
-        switch(operacao){
-        case "+": resultado = primeiroOperando + segundoOperando;
-        break;
-        case "-": resultado = primeiroOperando - segundoOperando;
-        break;
-        case "*": resultado = primeiroOperando * segundoOperando;
-        break;
-        case "/": resultado = primeiroOperando / segundoOperando;     
-        break;
-        default: System.out.println("Digite um operador válido!");
+            }
+            primeiroOperando = Double.parseDouble(primeiro);
+
+            Object retorno = JOptionPane.showInputDialog(null,
+                    "Selecione uma Operação: ", "Operadores", JOptionPane.INFORMATION_MESSAGE,
+                    null, operacao, operacao[0]);
             
-        }
-       
-        System.out.printf("O resultado é: %.2f", resultado);
+            selectOperacao = String.valueOf(retorno);
+            
+            
+            terceiro : do{
+
+            segundo = JOptionPane.showInputDialog(null, "Digite o segundo operando");
+            segundoOperando = Double.parseDouble(segundo);
+            if(selectOperacao.equals("/") && segundoOperando == 0){
+               JOptionPane.showMessageDialog(null, "Não pode dividir por zero!");
+                
+            }else{
+                break terceiro;
+            }
+            }while(true);
+
+               switch (selectOperacao) {
+                case "+":
+                    resultado = primeiroOperando + segundoOperando;
+                    break;
+                case "-":
+                    resultado = primeiroOperando - segundoOperando;
+                    break;
+                case "*":
+                    resultado = primeiroOperando * segundoOperando;
+                    break;
+                case "/":
+                    resultado = primeiroOperando / segundoOperando;
+                    break;
+                default:
+                    System.out.println("Digite um operador válido!");
+
+            }
+            JOptionPane.showMessageDialog(null, "O resultado é " + resultado);
+            escolha = JOptionPane.showConfirmDialog(null, "Deseja continuar? ", "Atenção!",
+                     JOptionPane.YES_NO_OPTION);
+        } while (escolha == 0);
+        JOptionPane.showMessageDialog(null, "Obrigado por usar a calculadora!");
+
     }
-    
+
 }
